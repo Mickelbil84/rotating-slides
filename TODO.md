f4819c0a4f1627b3bc7851140a2153da0d56dd66
Michael Bilevich <mickelbil84@gmail.com>
2025-02-02 00:28:51 +0200

    Update TODO

--- TODO.md
+++ TODO.md
@@ -1,4 +1,5 @@
-[ ] Make lastModified also as tinyMCE
-[ ] Add duration (seconds) key
+[x] Make lastModified also as tinyMCE
+[x] Add duration (seconds) key
 [ ] Add single template that has all slides as hidden divs, and javascript that reveals them in time
+[ ] Add ticking counter on bottom left of the slide ("Time to next slide-xx")
 [ ] Think about auto-refresh
--- index/models.py
+++ index/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 class Slide(models.Model):
     index = models.IntegerField()
+    duration = models.IntegerField()
     title = models.CharField(max_length=100)
-    last_updated = models.CharField(max_length=100)
+    last_updated = models.TextField()
     content = models.TextField()
 
     def __str__(self):
