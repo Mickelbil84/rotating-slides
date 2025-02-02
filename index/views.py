f13f757fd7c83a3adee1e23264e48ab9f6b7d2e9
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-02-02 15:49:22 +0200

    Add the rotating slides logic

--- index/views.py
+++ index/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Slide
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {"slides": Slide.objects.order_by("index")})
---
+++ static/js/rotating_slides.js
@@ -0,0 +1,41 @@
+var slideMaster = {
+    slides: document.querySelectorAll('.slide'),
+    currentSlide: 0,
+    timer: null,
+    interval: null,
+    disableAllSlides: function() {
+        this.slides.forEach(slide => slide.style.display = 'none');
+    },
+    enableSlide: function(slideIndex) {
+        this.disableAllSlides();
+        this.slides[slideIndex].style.display = 'block';
+
+        clearTimeout(this.timer);
+        clearInterval(this.interval);
+        var duration = parseInt(this.slides[slideIndex].getAttribute('slide-duration'), 10) * 1000;
+        this.timer = setTimeout(() => this.nextSlide(), duration);
+        var counter = duration / 1000;
+        this.getSlideDurationSpan(slideIndex).textContent = counter;
+        this.interval = setInterval(() => {
+            counter -= 10;
+            if (counter >= 0) {
+                this.getSlideDurationSpan(slideIndex).textContent = counter;
+            }
+        }, 10000);
+    },
+    nextSlide: function() {
+        this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+        this.enableSlide(this.currentSlide);
+    },
+    previousSlide: function() {
+        this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+        this.enableSlide(this.currentSlide);
+    },
+    getSlideDurationSpan: function(slideIndex) {
+        return this.slides[slideIndex].querySelector('#next-slide-cntr');
+    },
+};
+slideMaster.enableSlide(slideMaster.currentSlide);
+
+document.querySelector('#btn-next').addEventListener('click', () => slideMaster.nextSlide());
+document.querySelector('#btn-prev').addEventListener('click', () => slideMaster.previousSlide());
